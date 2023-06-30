531f91ae4a9e1ab21270a5653321483e9328865a
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-01 01:43:24 +0530

    Initial exerciseDetail Section Done

--- src/components/exercisedetail.jsx
+++ src/components/exercisedetail.jsx
@@ -1,8 +1,59 @@
 import React from 'react'
+import { Typography,Button,Stack } from '@mui/material'
+import BodyPartImage from '../assets/icons/body-part.png'
+import TargetImage from '../assets/icons/target.png'
+import EquipmentImage from '../assets/icons/equipment.png'
+import BodyPart from './bodypart'
 
-function ExerciseDetails() {
+function ExerciseDetails({exerciseDetail}) {
+    const {bodyPart , gifUrl ,name,target ,equipment}= exerciseDetail;
+    const extraDetail =[{
+        icon: BodyPartImage,
+        name :bodyPart,
+    },{
+        icon: EquipmentImage,
+        name :equipment,
+    },{
+        icon: TargetImage,
+        name :target,
+    },]
+    console.log(gifUrl);
   return (
-    <div>ExerciseDetails</div>
+   <>
+ <Stack gap="60px" sx={{
+    flexDirection:{lg: 'row'},padding:'20px',alignItems: 'center'
+    }}
+    >
+  <img src={gifUrl} alt={name} loading='lazy' className='detail-image'/>
+<Stack sx={{
+    gap: {lg:'35px', xs : '20px'}
+}}>
+<Typography variant='h3' textTransform="capitalize">
+    {name}
+</Typography>
+<Typography variant='h6' textTransform="capitalize">
+    Exercises Keep you strong . {name} {' '}
+    is one of the best exercises to target on your {target} .It willhelp you improve your mood and gain energy.
+</Typography>
+{extraDetail.map((item) =>{
+    return(
+    <Stack key={item.name} direction="row" gap="24px" alignItems="center">
+    <Button sx={{
+        background:'yellow', borderRadius:'50%', width:'80px',height:'80px'
+    }}>
+<img src={item.icon} alt={bodyPart} style={{width :'40px', height:'40px'}}/>
+
+    </Button>
+    <Typography variant='h6' fontWeight="550" textTransform="capitalize">
+     {item.name}
+    </Typography>
+
+    </Stack>)
+})}
+</Stack>
+ </Stack>
+
+   </>
   )
 }
 
--- src/pages/exercisedetails.jsx
+++ src/pages/exercisedetails.jsx
@@ -8,10 +8,26 @@ import ExerciseVideos from '../components/exercisevideos.jsx';
 import SimilarExercises from '../components/similarexercises.jsx';
 
 function ExerciseDetails() {
+  const [exerciseDetail ,setExerciseDetail]= useState({});
+  const {id} = useParams();
+
+  useEffect(() => {
+  const fetchExercisesData = async()=>{
+    const exerciseDbUrl = "https://exercisesdb.p.rapidapi.com";
+    const youtubeSearchUrl ="https://youtube-search-and-download.p.rapidapi.com/channel/about"
+
+    const exerciseDetailData =await  fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+    setExerciseDetail(exerciseDetailData);
+  }
+  fetchExercisesData();
+  }, [id])
+  
+
   return (
+   
   <>
   <Box>
-   <Detail />
+   <Detail exerciseDetail={exerciseDetail}/>
    <ExerciseVideos  />
    <SimilarExercises />
 
