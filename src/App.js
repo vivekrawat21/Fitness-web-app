3c22cef611cced4f9c4541af8826966cb6f8d7d9
Saurav Pant <pantsaurav005@gmail.com>
2023-07-02 12:52:14 +0530

    Footer Renamed in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from "react";
 import { Box } from "@mui/material";
-import './App.css'
-import { Routes ,Route } from "react-router-dom";
-import Home from "./pages/home"
-import Navbar from "./components/navbar"
-import ExerciseDetails from "./pages/exercisedetails"
-import Fotter from "./components/fotter"
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Navbar from "./components/navbar";
+import ExerciseDetails from "./pages/exercisedetails";
+import Footer from "./components/footer";
 function App() {
-  
   return (
     <>
-     <Box width ="400px" sx={{width:{xl:'1488px'}}} m="auto">
-    <Navbar />
-    <Routes>
-    
-    <Route path="/" element={<Home />}/>
-    <Route path="/exercisedetails/:id" element= {<ExerciseDetails />}/>
-
-    </Routes>
-    <Fotter />
-
-     </Box> 
+      <Box width="400px" sx={{ width: { xl: "1488px" } }} m="auto">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/exercisedetails/:id" element={<ExerciseDetails />} />
+        </Routes>
+        <Footer />
+      </Box>
     </>
   );
 }
