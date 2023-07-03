25e99cfa45f8488685be1021ff2dfce399a8e674
Saurav Pant <pantsaurav005@gmail.com>
2023-07-03 22:54:09 +0530

    Ruf Context code added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@emotion/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-   <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  
-  
+  <BrowserRouter>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </BrowserRouter>
 );
-
-
