d2a0fafbb57dbb9394f38ecb095936ec25191999
Saurav Pant <pantsaurav005@gmail.com>
2023-07-03 23:00:48 +0530

    some Bug Fixed Related to Theme

--- src/Context/Theme.js
+++ src/Context/Theme.js
@@ -1,28 +1,26 @@
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 
 const ThemeContext = createContext();
+
+const LightTheme = {
+  background: "#fff",
+  color: "#000",
+};
+
+const DarkTheme = {
+  background: "#000",
+  color: "#fff",
+};
 
 const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(LightTheme);
 
-  //   Rough Colors
-
-  const LightTheme = {
-    background: "#fff",
-    color: "#000",
-  };
-
-  const DarkTheme = {
-    background: "#000",
-    color: "#fff",
-  };
-
-  const ToggleTheme = () => {
+  const toggleTheme = () => {
     setTheme(theme === LightTheme ? DarkTheme : LightTheme);
   };
 
   return (
-    <ThemeContext.Provider value={{ theme, ToggleTheme }}>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
       {children}
     </ThemeContext.Provider>
   );
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "@emotion/react";
+import { ThemeProvider } from "../src/Context/Theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
