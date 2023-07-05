ebecab600822cca9a1d2b9149b202a1f1db95d3b
Saurav Pant <pantsaurav005@gmail.com>
2023-07-05 09:23:22 +0530

    rough

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,15 @@
-import { Link, useLocation } from 'react-router-dom';
-import Logo from '../assets/images/Logo.png';
-import { ThemeContext } from '../Context/Theme';
-import React, { useState, useContext, useEffect } from 'react';
-import { AppBar, Toolbar, Tabs, Tab, Button } from '@mui/material';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import Drawer from './Drawer';
+import { Link, useLocation } from "react-router-dom";
+import Logo from "../assets/images/Logo.png";
+import { ThemeContext } from "../Context/Theme";
+import React, { useState, useContext, useEffect } from "react";
+import { AppBar, Toolbar, Tabs, Tab, Button } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import Drawer from "./Drawer";
 
 const Navbar = () => {
   const location = useLocation();
   const { theme, toggleTheme } = useContext(ThemeContext);
-  const [value, setValue] = useState('/');
+  const [value, setValue] = useState("/");
 
   useEffect(() => {
     setValue(location.pathname);
@@ -21,48 +21,56 @@ const Navbar = () => {
 
   const handleExercisesClick = (event) => {
     event.preventDefault();
-    const exercisesSection = document.getElementById('exercises');
-    exercisesSection.scrollIntoView({ behavior: 'smooth' });
+    const exercisesSection = document.getElementById("exercises");
+    exercisesSection.scrollIntoView({ behavior: "smooth" });
     window.scrollTo(0, 900);
   };
 
   const Pages = [
-    { label: 'Home', path: '/' },
-    { label: 'Exercises', path: '/exercises' },
-    { label: 'Blogs', path: '/blogs' },
+    { label: "Home", path: "/" },
+    { label: "Exercises", path: "/exercises" },
+    { label: "Blogs", path: "/blogs" },
   ];
-const drawerPages = ['Home' , 'Exercises' ,'blogs']
+  const drawerPages = ["Home", "Exercises", "blogs"];
   return (
     <>
       <AppBar
         sx={{
           backgroundColor: theme.color,
-          opacity: '0.9',
-          position: 'sticky',
+          opacity: "0.9",
+          position: "sticky",
           top: 0,
         }}
       >
         <Toolbar>
-          <Link to="/" onClick={()=>{
-            window.scrollTo(0,0)
-          }}>
+          <Link
+            to="/"
+            onClick={() => {
+              window.scrollTo(0, 0);
+            }}
+          >
             <img
               src={Logo}
               alt="Logo"
               style={{
-                width: '48px',
-                height: '48px',
-                margin: '0 20px',
+                width: "48px",
+                height: "48px",
+                margin: "0 20px",
               }}
             />
           </Link>
 
           <Tabs
             textColor="white"
-            TabIndicatorProps={{ style: { background: '#6339f5', height: '3.5px' } }}
+            TabIndicatorProps={{
+              style: { background: "#6339f5", height: "3.5px" },
+            }}
             value={value}
             onChange={handleTabChange}
-            sx={{ marginLeft: 'auto', visibility: { lg: 'visible', xs: 'hidden' } }}
+            sx={{
+              marginLeft: "auto",
+              visibility: { lg: "visible", xs: "hidden" },
+            }}
           >
             {Pages.map((page) => (
               <Tab
@@ -70,14 +78,16 @@ const drawerPages = ['Home' , 'Exercises' ,'blogs']
                 label={page.label}
                 value={page.path}
                 sx={{
-                  fontWeight: '900',
-                  fontSize: '17px',
-                  margin: '0px 15px',
+                  fontWeight: "900",
+                  fontSize: "17px",
+                  margin: "0px 15px",
                   color: theme.background,
                 }}
                 component={Link}
                 to={page.path}
-                onClick={page.path === '/exercises' ? handleExercisesClick : null}
+                onClick={
+                  page.path === "/exercises" ? handleExercisesClick : null
+                }
               />
             ))}
           </Tabs>
@@ -85,29 +95,26 @@ const drawerPages = ['Home' , 'Exercises' ,'blogs']
             onClick={toggleTheme}
             sx={{
               color: theme.background,
-              borderRadius: '100px',
-              marginLeft: 'auto',
+              borderRadius: "100px",
+              marginLeft: "auto",
             }}
           >
             {theme.icon}
           </Button>
-          
+
           <Button
             sx={{
-              backgroundColor: 'inherit',
+              backgroundColor: "inherit",
               color: theme.background,
-              width: '5px',
-              borderRadius: '100px',
-              marginLeft: '5px',
+              width: "5px",
+              borderRadius: "100px",
+              marginLeft: "5px",
             }}
           >
-          
             <GitHubIcon />
           </Button>
-          
 
           <Drawer Pages={drawerPages} />
-         
         </Toolbar>
       </AppBar>
     </>
