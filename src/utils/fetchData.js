be593ec86a26cdb1bb80a84606f97230aa5033cd
Saurav Pant <pantsaurav005@gmail.com>
2023-07-06 13:44:23 +0530

    First Page animation added

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,24 +1,21 @@
 export const exerciseOptions = {
-    method: 'GET',
-    url: 'https://exercisedb.p.rapidapi.com/exercises/bodyPartList',
+  method: "GET",
+  url: "https://exercisedb.p.rapidapi.com/exercises/bodyPartList",
   headers: {
-    'X-RapidAPI-Key': '2fd8c6733amsh318f79c96cc5087p128c61jsn3337a3caa29b',
-    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    "X-RapidAPI-Key": "deb58588a6msh552df0188e83e94p173e41jsn03c38b000981",
+    "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
+  },
+};
+export const youtubeOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "1bb3eaa827msh4dcfa512ff64662p14724cjsn4dbbf6286175",
+    "X-RapidAPI-Host": "youtube-search-and-download.p.rapidapi.com",
+  },
+};
 
-    }
-  };
-   export const youtubeOptions = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '1bb3eaa827msh4dcfa512ff64662p14724cjsn4dbbf6286175',
-      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
-    }
-  };
-
-export const fetchData = async(url,options)=>{
-    const response  = await fetch(url,options);
-    const data = await response.json();
-    return data;
-}
-
-  
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+  return data;
+};
