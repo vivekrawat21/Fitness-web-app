e0a767387db9dc82e091e4d996454c54eb6b43e3
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-11 13:10:14 +0530

    added a blog page initial

--- src/pages/blogs.jsx
+++ src/pages/blogs.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Stack,Box } from '@mui/material';
+import Card from '../components/card';
 
  const blogs = () => {
   return (
-    <div>blogs</div>
+    <>
+    
+        <Stack direction={'column'} justifyContent={"center"} alignItems={"center"} sx={{marginTop:"20px"}} >
+             
+          <Card />
+
+
+        </Stack>
+    </>
+
+
   )
 }
 
