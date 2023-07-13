9f0db85f98de8be3643da2cd68dc43da52d7b7a0
vivekrawat21 <vivekrwt2111@gmail.com>
2023-07-13 22:02:55 +0530

    added some framer motion to fotter

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,116 +1,285 @@
 import React from "react";
 
 import Logo1 from "../assets/images/lo.png";
-import ReactLogo from "../assets/icons/react-logo-1.png"
-import RapidApi from "../assets/icons/rapidapi-icon.svg"
-import { Box, Stack, Typography ,Button } from "@mui/material";
+import ReactLogo from "../assets/icons/react-logo-1.png";
+import RapidApi from "../assets/icons/rapidapi-icon.svg";
+import { Box, Stack, Typography, Button } from "@mui/material";
 
 import { Link } from "react-router-dom";
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import { motion } from "framer-motion";
 
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import MaterialUi from "../assets/icons/material-ui-1.svg";
+import LaunchIcon from "@mui/icons-material/Launch";
 
 const Footer = () => {
   return (
     <Box mt="80px" bgcolor="#111827">
-   <Stack direction="row"  justifyContent="space-between" alignItems={"center"} mx={{lg:"40px",xs:"20px"}}>
-      <Stack  alignItems="center" direction="column" spacing={{  lg :2 ,xs: 1, sm: 2, md: 4 }} >
-        <Typography  sx={{ fontWeight:"660", fontSize:{lg:"25px",xs:"16px"},'&:hover':{borderBottom:"2px solid white "}}} color="white">
-          Social links
-        </Typography>
-        <div style={{display:"flex"}}>
-      <a href="www.linkedin.com" target="#">
-            <Button
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+        alignItems={"center"}
+        mx={{ lg: "40px", xs: "20px" }}
+      >
+        <Stack
+          alignItems="center"
+          direction="column"
+          spacing={{ lg: 2, xs: 1, sm: 2, md: 4 }}
+        >
+          <Typography
+            component={motion.div}
+            sx={{
+              fontWeight: "660",
+              fontSize: { lg: "25px", xs: "16px" },
+              "&:hover": { borderBottom: "2px solid white " },
+            }}
+            color="white"
+            initial={{
+              opacity: 0,
+              x: 100,
+            }}
+            whileInView={{
+              opacity: 1,
+              x: 0,
+            }}
+            transition={{
+              duration: 0.8,
+              delay: 0.3,
+            }}
+          >
+            Social links
+          </Typography>
+          <motion.div
+            style={{ display: "flex" }}
+            component={motion.div}
+            initial={{
+              opacity: 0,
+              y: 100,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{
+              duration: 1,
+              delay: 0.3,
+            }}
+          >
+            <a href="www.linkedin.com" target="#">
+              <Button
+                sx={{
+                  backgroundColor: "inherit",
+                  color: "white",
+                  borderRadius: "100px",
+                  marginLeft: { lg: "5px", xs: "3px" },
+                  "&:hover": {
+                    transform: "translateY(-5px)",
+                  },
+                }}
+              >
+                <LinkedInIcon />
+              </Button>
+            </a>
+            <a href="www.twitter.com" target="#">
+              <Button
+                sx={{
+                  backgroundColor: "inherit",
+                  color: "white",
+                  borderRadius: "100px",
+                  marginLeft: { lg: "5px", xs: "3px" },
+                  "&:hover": {
+                    transform: "translateY(-5px) ",
+                  },
+                }}
+              >
+                <TwitterIcon />
+              </Button>
+            </a>
+            <a href="www.facebook.com" target="#">
+              <Button
+                sx={{
+                  backgroundColor: "inherit",
+                  color: "white",
+                  borderRadius: "100px",
+
+                  "&:hover": {
+                    transform: "translateY(-5px) ",
+                  },
+                }}
+              >
+                <FacebookIcon />
+              </Button>
+            </a>
+          </motion.div>
+        </Stack>
+        <Stack>
+          <motion.img
+            src={Logo1}
+            alt="Logo"
+            width="200px"
+            height="40px"
+            className="logo-1"
+            initial={{
+              opacity: 0,
+              y: -100,
+            }}
+            whileInView={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{
+              duration: 0.8,
+              delay: 0.3,
+            }}
+          ></motion.img>
+        </Stack>
+        <Stack
+          direction="column"
+          alignItems="center"
+          marginBottom="10px"
+          spacing={{ xs: 1, sm: 1, md: 2 }}
+        >
+          <Typography
+            color="white"
+            component={motion.div}
+            sx={{
+              fontSize: { lg: "25px", xs: "16px" },
+              fontWeight: "bold",
+              marginRight: "40px",
+              marginTop: "10px",
+              "&:hover": { borderBottom: "2px solid white " },
+            }}
+            initial={{
+              opacity: 0,
+              x: 100,
+            }}
+            whileInView={{
+              opacity: 1,
+              x: 0,
+            }}
+            transition={{
+              duration: 0.8,
+              delay: 0.3,
+            }}
+          >
+            Important links
+          </Typography>
+
+          <Link
+            to="/"
+            onClick={() => {
+              window.scrollTo(0, 0);
+            }}
+            style={{ textDecoration: "none" }}
+          >
+            <Typography
+              fontFamily="monospace"
+              component={motion.div}
               sx={{
-                backgroundColor: "inherit",
-                color:"white",
-                width: {lg:"10px",xs:"3px"},
-                height: {lg:"10px",xs:"3px"},
-                borderRadius: "100px",
-                marginLeft: {lg:"5px",xs:"3px"},
-                '&:hover': {
-               
-                  transform: 'translateY(-5px)',
-                },
+                color: "white",
+                fontSize: { lg: "15px", xs: "9px" },
+                fontWeight: "bold",
+              }}
+              initial={{
+                opacity: 0,
+                y: 100,
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+              }}
+              transition={{
+                duration: 1,
+                delay: 0.3,
               }}
             >
-              <LinkedInIcon />
-            </Button>
-          </a>
-          <a href="www.twitter.com" target="#">
-            <Button
+              Home {<LaunchIcon fontSize="10px" />}
+            </Typography>
+          </Link>
+          <Link
+            to="/blogs"
+            style={{ textDecoration: "none" }}
+            onClick={() => {
+              window.scrollTo(0, 0);
+            }}
+          >
+            <Typography
+              fontFamily="monospace"
+              component={motion.div}
               sx={{
-                backgroundColor: "inherit",
-                color:"white",
-                width: {lg:"10px",xs:"3px"},
-                height: {lg:"10px",xs:"3px"},
-                borderRadius: "100px",
-                marginLeft:{lg:"5px",xs:"3px"},
-                '&:hover': {
-                  
-                  transform: 'translateY(-5px) ',
-                },
-              }}
-            >
-              <TwitterIcon />
-            </Button>
-          </a>
-          <a href="www.facebook.com" target="#">
-            <Button
-              sx={{
-                backgroundColor: "inherit",
-                color:"white",
-                width: {lg:"10px",xs:"3px"},
-                height: {lg:"10px",xs:"3px"},
-                borderRadius: "100px",
-              
-                '&:hover': {
+                color: "white",
+                fontSize: { lg: "15px", xs: "9px" },
+                fontWeight: "bold",
                 
-                  transform: 'translateY(-5px) ',
-                },
+              }}
+              initial={{
+                opacity: 0,
+                y: 100,
+              }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+              }}
+              transition={{
+                duration: 1,
+                delay: 0.3,
               }}
             >
-              <FacebookIcon />
-            </Button>
-          </a>
-          </div>
-      </Stack>
-      <Stack>
-        <img src={Logo1} alt="Logo" width="200px" height="40px" className="logo-1"/>
-        
-      </Stack>
-      <Stack direction="column"   alignItems="center" marginBottom="10px" spacing={{ xs: 1, sm: 1, md: 2 }}>
-      <Typography  color="white" sx={{ fontSize:{lg:"25px",xs:"16px"},fontWeight:"bold", marginRight:"40px" ,marginTop:"10px",'&:hover':{borderBottom:"2px solid white "}}}>
-          Important links
-        </Typography>
-      
-      <Link to="/"  onClick={() => {
-        
-              window.scrollTo(0, 0);
-            }} style={{ textDecoration: 'none' }} 
-            >
-           
-        <Typography sx={{  color :"white" ,fontSize:{lg:"17px",xs:"11px"},fontWeight:"520", '&:hover': {
-                  transform: 'translateY(-5px)'
-                }}} >Home</Typography>
+              Blogs {<LaunchIcon fontSize="10px" />}
+            </Typography>
           </Link>
-          <Link to="/blogs" style={{textDecoration:"none"}} onClick={() => {
-        
-              window.scrollTo(0, 0);
-            }}>
-<Typography  sx={{  color :"white" , fontSize:{lg:"17px",xs:"11px"}, fontWeight:"520",'&:hover': {
-                  transform: 'translateY(-5px)',
-                }}}>Blogs</Typography>
-          </Link>
+        </Stack>
       </Stack>
-      </Stack>
-    <div style={{display:"flex" , background:"#100C08", color:"white" , width:"100vw" ,justifyContent:"center", alignItems:"center",height:"50px" , fontWeight:900 ,fontFamily:"fantasy", fontSize:"17px"}}>
-       All rights &copy; are reserved  Made With 💖 and <img src={ ReactLogo} alt="react" width="30px" style={{marginLeft:"13px", marginRight:"10px"}}/> <img src={ MaterialUi} alt="materialui" width="30px" style={{marginTop:"5px" ,marginRight:"10px",marginLeft:"10px"}}  /> <img src={ RapidApi} alt="rapid" width="30px" style={{marginRight:"13px", marginLeft:"13px"}}/>
+      <motion.div
+      initial={{
+              opacity: 0,
+              x: -200,
+            }}
+            whileInView={{
+              opacity: 1,
+              x: 0,
+            }}
+            transition={{
+              duration: 1.0,
+              delay: 0.3,
+            }}
+        style={{
+          display: "flex",
+          background: "#100C08",
+          color: "white",
+          width: "100vw",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "50px",
+          fontWeight: 900,
+          fontFamily: "fantasy",
+          fontSize: "17px",
           
-          </div>
-     
+        }}
+       
+      >
+        All rights &copy; are reserved Made With 💖 and{" "}
+        <img
+          src={ReactLogo}
+          alt="react"
+          width="30px"
+          style={{ marginLeft: "13px", marginRight: "10px" }}
+        />{" "}
+        <img
+          src={MaterialUi}
+          alt="materialui"
+          width="30px"
+          style={{ marginTop: "5px", marginRight: "10px", marginLeft: "10px" }}
+        />{" "}
+        <img
+          src={RapidApi}
+          alt="rapid"
+          width="30px"
+          style={{ marginRight: "13px", marginLeft: "13px" }}
+        />
+      </motion.div>
     </Box>
   );
 };
